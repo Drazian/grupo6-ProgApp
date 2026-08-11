4b7d1e46e0b2a989cf1cc82b4808bbd7efaed285
Drazian <garridogabriel2001@gmail.com>
2026-08-11 06:44:59 -0300

    commit inicial

---
+++ src/main/java/com/edext/EdEXT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.edext;
+
+/**
+ *
+ * @author pipo
+ */
+public class EdEXT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
